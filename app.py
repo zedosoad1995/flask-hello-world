00e377b9153ac5bcc3927598ec2725bcd94a46a0
zedosoad1995 <53758908+zedosoad1995@users.noreply.github.com>
2024-12-04 01:57:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print("Request Headers:", request.headers)
+    print("Request Method:", request.method)
+    print("Request URL:", request.url)
+    print("Request Path:", request.path)
+    print("Request Args:", request.args)
+    print("Request Form:", request.form)
+    print("Request JSON:", request.get_json())
+    print("Request Remote Addr:", request.remote_addr)
+    print("Request Access Route:", request.access_route)
+    
     forwarded_for = request.headers.get('X-Forwarded-For')
     
     # Return it or print it to see the value
