225fc3cf51218039a2722bab33e6ce7bf1678df4
zedosoad1995 <53758908+zedosoad1995@users.noreply.github.com>
2024-12-04 01:56:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return f'Your IP is {request.remote_addr}'
+    forwarded_for = request.headers.get('X-Forwarded-For')
+    
+    # Return it or print it to see the value
+    if forwarded_for:
+        return f'X-Forwarded-For header: {forwarded_for}'
+    else:
+        return 'X-Forwarded-For header is not set'
